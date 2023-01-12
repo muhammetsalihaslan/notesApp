38df5652a836b6b4b548bae8cde93ba3847af872
salih <muhammetsalihaslan@gmail.com>
2023-01-12 15:06:05 +0300

    css done

--- notesapp/src/components/Home/Home.jsx
+++ notesapp/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import EnterNote from "../enterNote/EnterNote";
 import Search from "../search/Search";
 
 const Home = () => {
   return (
     <div className="home">
       <Search />
+      <EnterNote />
     </div>
   );
 };
--- notesapp/src/components/enterNote/EnterNote.jsx
+++ notesapp/src/components/enterNote/EnterNote.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const EnterNote = () => {
-  return <div></div>;
+  return (
+    <form className="enter">
+      <textarea className="Itext">Some text</textarea>
+      <div className="buttons">
+        <div className="buttons1">
+          <input className="btn btn1" type="button" />
+          <input type="button" className="btn btn2" />
+          <input type="button" className="btn btn3" />
+          <input type="button" className="btn btn4" />
+          <input type="button" className="btn btn5" />
+        </div>
+        <div className="buttons2">
+          <button className="addB">ADD</button>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default EnterNote;
