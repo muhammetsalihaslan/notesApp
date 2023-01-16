655714a576ad4f150e6c0d3c64a54211593f08a7
salih <muhammetsalihaslan@gmail.com>
2023-01-16 22:01:10 +0300

    noteList

--- notesapp/src/components/noteList/NoteList.jsx
+++ notesapp/src/components/noteList/NoteList.jsx
@@ -4,11 +4,18 @@ const NoteList = () => {
   return (
     <div className="noteList">
       <div className="note">
-        <div className="noteF">Note</div>
-        <div>
-          <label className="noteT">title</label>
-          <input type="checkbox" />
-          <button></button>
+        <div className="note2">
+          <div className="noteF">Note</div>
+          <input className="toggle" type="checkbox" />
+          <label className="noteT">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Repellendus ipsam vel facilis debitis quam, nam tempora. Qui vitae
+            iste reprehenderit odit corporis? Debitis porro iste quibusdam quam,
+            accusantium repellendus explicabo unde neque in consequuntur
+            consectetur ducimus vitae laborum tenetur excepturi autem dolor
+            nulla iusto qui, natus at et. Doloremque, quod!
+          </label>
+          <button className="destroy"></button>
         </div>
       </div>
     </div>
