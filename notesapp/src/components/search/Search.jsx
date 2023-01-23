b82a8ac017bc123875857f64733f0bc566137496
salih <muhammetsalihaslan@gmail.com>
2023-01-23 15:40:57 +0300

    filter

--- notesapp/src/components/search/Search.jsx
+++ notesapp/src/components/search/Search.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useState } from "react";
+import { filtered } from "../../redux/todos/todoSlice";
+import { useDispatch } from "react-redux";
 
 const Search = () => {
+  const [filterText, setFilterText] = useState("");
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <input className="search" type="text" placeholder="Search..." />
+      <input
+        className="search"
+        type="text"
+        placeholder="Search..."
+        value={filterText}
+        onChange={(e) => setFilterText(dispatch(filtered()))}
+      />
     </div>
   );
 };
